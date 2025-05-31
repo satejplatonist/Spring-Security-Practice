4039bdef0dbc54cb9e1a65aa4bce06b27fd10f5b
satejplatonist <satejshambho@gmail.com>
2025-05-31 22:36:04 +0530

    spring security practice - oauth resource server using jwt support

---
+++ oauth-practice/ResourceServer/ResourceServer/src/main/java/OAuth2/ResourceServer/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package OAuth2.ResourceServer.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig 
+{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+    	http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+    	    .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+    	return http.build();
+    }
+    
+    @Bean
+    public JwtDecoder decoder()
+    {
+    	return JwtDecoders.fromIssuerLocation("https://my-auth-server.com");
+    }
+}
