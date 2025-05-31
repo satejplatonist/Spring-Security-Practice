39a620cf8497a57cf819fbbd75889a23f9706cf0
satejplatonist <satejshambho@gmail.com>
2025-05-31 23:54:40 +0530

    a folder deleted and client practice folder created

---
+++ oauth-practice/Client/Client/src/test/java/OAuth2/Client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package OAuth2.Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
